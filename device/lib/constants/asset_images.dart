18c9b318063ba21595ece55d06b91b82583bf676
Trịnh Thị Thương <100211014+thuong1506@users.noreply.github.com>
2023-06-08 20:20:42 +0700

    Delete device directory

--- device/lib/constants/asset_images.dart
+++
@@ -1,8 +0,0 @@
-class AssetsImages {
-  static AssetsImages instance = AssetsImages();
-
-  static const String _imagesPath = "assets/images";
-
-  final String welcomeImage = "$_imagesPath/welcome.png";
-  final String googleLogo = "$_imagesPath/googlelogo.png";
-}
